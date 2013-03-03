5b6d98fab111103fb01d1ac992a0e7f0e2dc6d17
JP Bochi <jpbochi@thoughtworks.com>
2013-03-03 04:58:37 -0300

    update halberd. now it supports AMD

--- client.js
+++ client.js
@@ -1,10 +1,8 @@
 define(function (require) {
-  /*global hal */
-
   var client = require('./lib/client/api_client.js').extendHal();
 
   client.get('/api').then(function (root) {
-    window.api = root;
+    window.api = root._links;
   }).fail(function (jqXHR) {
     throw 'Failed to load api';
   });
