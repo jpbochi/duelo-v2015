726822a40816e5a54bed4543654cbb61c8af539d
JP Bochi <jpbochi@thoughtworks.com>
2013-04-07 01:36:00 -0300

    moved lobby tests to separate file

--- tests/api.js
+++ tests/api.js
@@ -2,4 +2,5 @@ define(function (require) {
   require('/tests/api/root_tests.js');
   require('/tests/api/game_lists_tests.js');
   require('/tests/api/game_tests.js');
+  require('/tests/api/game_lobby_tests.js');
 });
