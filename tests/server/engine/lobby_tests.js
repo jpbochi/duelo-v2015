71c8daabe9a73e99c7592f1c7dda841f2eddb69b
JP Bochi <jpbochi@gmail.com>
2013-05-06 23:05:04 -0400

    added test for lobby state machine on server side

---
+++ tests/server/engine/lobby_tests.js
@@ -0,0 +1,25 @@
+define(function (require) {
+  var _ = require('lodash');
+  var sinon = require('sinon-restore');
+  var should = require('tests/support/should.js');
+  var lobby = require('lib/server/engine/lobby.js');
+
+  var newGame = function (options) {
+    return mongo.games.create({
+      buildOptions: options
+    });
+  };
+  var fakeUser = function (name, id) {
+    return { displayName: name, _links: { self: { href: '#/player/' + id } } };
+  };
+
+  QUnit.module('lobby().join()');
+
+  test('adds player to game', function () {
+    var game = newGame();
+
+    lobby(game, fakeUser('le player')).join();
+
+    deepEqual(_.pluck(game.players, 'displayName'), [ 'le player' ]);
+  });
+});
